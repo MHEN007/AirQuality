66aac8dfaf32e2cfa47d3300bc50c904e9e1b8f0
MHEN007 <matthew.mahendra@gmail.com>
2024-09-18 10:35:36 +0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Proyek Analisis Data: Air Quality Dataset 
+- **Nama:** Matthew Mahendra
+- **Email:** m002b4ky2431@bangkit.academy
+- **ID Dicoding:** matt_mahen
+
+## Notebook
+The notebook contains data wrangling, EDA, and visualization of the Air Quality Dataset. It is available in the root of the project with the filename notebook.ipynb
+
+## Dashboard
+The dashboard given here is build using Streamlit using the final data from the notebook. To run, use the following command from the `dashboard` folder.
+
+```
+python -m streamlit run main.py
+```
+
+The dashboard contains the two business questions from the notebook. Some of the visualization in the dashboard is able to be modified for some of the parameters.
---
+++ dashboard/main.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('main_data.csv')
+
+st.title("Dashboard Kualitas Udara")
+
+tab_air_index, tab_tren_udara = st.tabs(["Air Quality Index", "Pollutants Trend"])
+
+with tab_air_index:
+    st.header("Air Quality Index")
+    city_air_quality = df.groupby(['station', 'air_quality']).size().reset_index(name='count')
+
+    plt.figure(figsize=(20,10))
+
+    sns.barplot(x='station', y='count', hue='air_quality', data=city_air_quality)
+    plt.title('Air Quality Index by City')
+    
+    st.pyplot(plt)
+
+    st.write("Based on NO2, SO2, O3, PM2.5, and PM10 values")
+
+
+with tab_tren_udara:
+    st.header("Pollutants Trend")
+
+    selection = st.selectbox("Please choose pollutant", ['PM2.5', 'PM10', 'NO2', 'SO2', 'CO', 'O3', 'TEMP', 'PRES'])
+    year_start = st.selectbox("Please choose start year", [2013, 2014, 2015, 2016, 2017])
+    year_end = st.selectbox("Please choose end year", [2013, 2014, 2015, 2016, 2017])
+
+    filtered_data = df[(df['year'] >= 2013) & (df['year'] <= year_end)]
+    plt.figure(figsize=(20, 10))
+    sns.lineplot(filtered_data, x='hour', y=selection)
+    plt.title(f"Trend of {selection} from 2013-{year_end}")
+    st.pyplot(plt)
